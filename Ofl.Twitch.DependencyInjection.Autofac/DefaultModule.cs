8a0a2d51999a432f33780da70817b2ff41a615a1
casperOne <casperOne@caspershouse.com>
2017-03-02 11:31:13 -0500

    Initial commit.

---
+++ Ofl.Twitch.DependencyInjection.Autofac/DefaultModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Autofac;
+using Ofl.Twitch.V5;
+
+namespace Ofl.Twitch.DependencyInjection.Autofac
+{
+    public class DefaultModule : Module
+    {
+        #region Overrides of Module
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            // Validate parameters.
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            // Bind.
+            builder.RegisterType<TwitchUtilities>().As<ITwitchUtilities>();
+            builder.RegisterType<TwitchClient>().As<ITwitchClient>();
+            builder.RegisterType<ConfigurationClientIdProvider>().As<IClientIdProvider>();
+        }
+
+        #endregion
+    }
+}
---
+++ Ofl.Twitch.DependencyInjection.Autofac/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Ofl.Twitch.DependencyInjection.Autofac")]
+[assembly: AssemblyProduct("Ofl.Twitch.DependencyInjection.Autofac")]
+[assembly: AssemblyDescription("Ofl.Twitch.Contract implementation registrations in Autofac.")]
